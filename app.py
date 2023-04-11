16b4f83c603a1ff66c7724f65fd3388df78da70e
Software26 <natanaelsosa@gmail.com>
2023-04-11 14:05:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask, redirect, render_template
+from flask import Flask,  render_template, request, redirect, url_for, session # pip install Flask
+from flask_mysqldb import MySQL,MySQLdb # pip install Flask-MySQLdb
+from os import path #pip install notify-py
+from notifypy import Notify
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'app_citas'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
